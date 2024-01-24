526ae7056ecf0791356e9880d45970dda8fc874f
HP <HP@DESKTOP-HOED7JD>
2024-01-24 17:35:54 +0530

    category is created

---
+++ src/test/java/com/PracticeMaven/CategoryTest.java
@@ -0,0 +1,12 @@
+package com.PracticeMaven;
+
+import org.testng.annotations.Test;
+
+public class CategoryTest {
+	@Test
+	public void categoryTest()
+	{
+		System.out.println("category created");
+	}
+
+}
